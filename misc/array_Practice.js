43fdac91d238df2037852b4b56634bcc276f4597
James Buniak <jamesbuniak@gmail.com>
2016-07-07 15:00:03 -0400

    moved files around

---
+++ misc/array_Practice.js
@@ -0,0 +1,8 @@
+ //  var boroughs = ['manhattan', 'the bronx', 'queens', 'brooklyn', 'staten island']
+
+ //  boroughs.forEach(function (borough) {
+	// console.log(borough +' stuff')
+ //  });
+
+
+ 
