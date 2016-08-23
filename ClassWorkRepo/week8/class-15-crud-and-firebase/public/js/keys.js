05816fba28c7a9c9be831019cf1775bd60a500af
James Buniak <jamesbuniak@gmail.com>
2016-08-23 19:28:00 -0400

    new files added to publicly served firebase

---
+++ ClassWorkRepo/week8/class-15-crud-and-firebase/public/js/keys.js
@@ -0,0 +1,8 @@
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAWkxTnFCwbxQQblR4zz3OevHMUWthpsv4",
+        authDomain: "korn-97621.firebaseapp.com",
+        databaseURL: "https://korn-97621.firebaseio.com",
+        storageBucket: "korn-97621.appspot.com",
+        };
+        firebase.initializeApp(config);
